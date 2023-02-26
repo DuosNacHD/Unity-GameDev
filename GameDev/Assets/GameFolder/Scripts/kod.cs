ec5a1f69f0dd8fee19354cf3c6f866a4b45ef4b4
DuosNacHD <erhan.turan206@gmail.com>
2023-02-26 15:09:36 +0300

    sa

---
+++ GameDev/Assets/GameFolder/Scripts/kod.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kod : MonoBehaviour
+{
+    // SelamEkrem
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
